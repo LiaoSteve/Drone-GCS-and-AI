fd23ad39e8dd000f3988a78c8149fa5ab89f8bb6
steve-Liao <steveliao1688@gmail.com>
2020-02-03 19:25:00 +0800

    --

--- django_test/blog/views.py
+++ django_test/blog/views.py
@@ -47,7 +47,7 @@ def eventsource(request):
                                
     return StreamingHttpResponse(event_stream(), content_type='text/event-stream')
 
-def web_cam(request):
+def web_cam(request):    
     s = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
     s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
     print('\nCam socket created')
@@ -56,34 +56,35 @@ def web_cam(request):
     s.listen(5)
     print('Cam socket now listening')
     conn, addr = s.accept()
-    print ("\n**********\nClient Info: ", conn, addr) 
-    def images_stream(conn, addr):        
+    print ("\n**********\nClient Info: ", conn, addr)      
+    def images_stream(conn,addr):        
         data = b""
         payload_size = struct.calcsize(">L")
         print("payload_size: {}".format(payload_size))
-        
         while True:
-            while len(data) < payload_size:
-                print("Recv: {}".format(len(data)))
-                data += conn.recv(4096)
-            print("Done Recv: {}".format(len(data)))
-            packed_msg_size = data[:payload_size]
-            data = data[payload_size:]
-            msg_size = struct.unpack(">L", packed_msg_size)[0]
-            print("msg_size: {}".format(msg_size))
+            try:
+                while len(data) < payload_size:
+                    print("Recv: {}".format(len(data)))
+                    data += conn.recv(4096)
+                print("Done Recv: {}".format(len(data)))
+                packed_msg_size = data[:payload_size]
+                data = data[payload_size:]
+                msg_size = struct.unpack(">L", packed_msg_size)[0]
+                print("msg_size: {}".format(msg_size))
 
-            while len(data) < msg_size:
-                data += conn.recv(4096)
-            frame_data = data[:msg_size]
-            data = data[msg_size:]
-            frame = pickle.loads(frame_data, fix_imports=True, encoding="bytes")
-            frame = cv2.imdecode(frame, cv2.IMREAD_COLOR)
-            _, frame = cv2.imencode('.jpg', frame)                                
-            yield (b'--frame\r\n'
-                b'Content-Type: image/jpeg\r\n\r\n' + (frame.tobytes()) + b'\r\n\r\n')
-            
-
-    return StreamingHttpResponse(images_stream(conn, addr),
+                while len(data) < msg_size:
+                    data += conn.recv(4096)
+                frame_data = data[:msg_size]
+                data = data[msg_size:]
+                frame = pickle.loads(frame_data, fix_imports=True, encoding="bytes")
+                frame = cv2.imdecode(frame, cv2.IMREAD_COLOR)
+                _, frame = cv2.imencode('.jpg', frame)                                
+                yield (b'--frame\r\n'
+                    b'Content-Type: image/jpeg\r\n\r\n' + (frame.tobytes()) + b'\r\n\r\n')
+            except Exception as e:
+                print(e)
+                
+    return StreamingHttpResponse(images_stream(conn,addr),
                      content_type='multipart/x-mixed-replace; boundary=frame') 
 
 

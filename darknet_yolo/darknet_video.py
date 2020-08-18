f85e61d9114c182514ef0436c732a7c184cdfdae
ti-2080 <steveliao1688@gmail.com>
2020-08-18 19:11:34 +0800

    videowriter

--- darknet_yolo/darknet_video.py
+++ darknet_yolo/darknet_video.py
@@ -1,5 +1,6 @@
 from ctypes import *
 import math
+import colorsys
 import random
 import os
 import cv2
@@ -7,33 +8,36 @@ import numpy as np
 import time
 import darknet
 
-def convertBack(x, y, w, h):
-    xmin = int( round( (x - (w / 2))* 640 / 416 ) )
-    xmax = int( round( (x + (w / 2))* 640 / 416 ) )
-    ymin = int( round( (y - (h / 2))* 480 / 416 ) )
-    ymax = int( round( (y + (h / 2))* 480 / 416 ) )
+def convertBack(x, y, w, h, *size_out):    
+    size = darknet.network_width(netMain)    
+    xmin = int( round( (x - (w / 2))* size_out[0][1] / size ) )
+    xmax = int( round( (x + (w / 2))* size_out[0][1] / size ) )
+    ymin = int( round( (y - (h / 2))* size_out[0][0] / size ) )
+    ymax = int( round( (y + (h / 2))* size_out[0][0] / size ) )
     return xmin, ymin, xmax, ymax
 
 
-def cvDrawBoxes(detections, img):
+def cvDrawBoxes(detections, img):        
+    num_classes = len(altNames)
+    hsv_tuples = [(1.0 * x / num_classes, 1., 1.) for x in range(num_classes)]
+    colors = list(map(lambda x: colorsys.hsv_to_rgb(*x), hsv_tuples))
+    colors = list(map(lambda x: (int(x[0] * 255), int(x[1] * 255), int(x[2] * 255)), colors))       
+    random.seed(68)    
     for detection in detections:
         x, y, w, h = detection[2][0],\
             detection[2][1],\
             detection[2][2],\
             detection[2][3]
         xmin, ymin, xmax, ymax = convertBack(
-            float(x), float(y), float(w), float(h))
+            float(x), float(y), float(w), float(h), img.shape)
         pt1 = (xmin, ymin)
         pt2 = (xmax, ymax)        
-        cv2.rectangle(img, pt1, pt2, (0, 255, 255), 1)      
-        cv2.putText(img,
-                    detection[0].decode() +
-                    " (" + str(int(detection[1] * 100)) + "%)",
+        bbox_color = colors[altNames.index(detection[0].decode())]
+        cv2.rectangle(img, pt1, pt2, bbox_color, 1)      
+        cv2.putText(img,f'{detection[0].decode()} ({int(detection[1] * 100)}%)',                    
                     (pt1[0], pt1[1] - 5), cv2.FONT_HERSHEY_COMPLEX_SMALL, 0.5,
                     [0, 0, 0], 6)
-        cv2.putText(img,
-                    detection[0].decode() +
-                    " (" + str(int(detection[1] * 100)) + "%)",
+        cv2.putText(img,f'{detection[0].decode()} ({int(detection[1] * 100)}%)',                    
                     (pt1[0], pt1[1] - 5), cv2.FONT_HERSHEY_COMPLEX_SMALL, 0.5,
                     [0, 255, 255], 1)            
     return img
@@ -66,11 +70,11 @@ def YOLO():
                 pass
     except Exception:
         pass
-    #cap = cv2.VideoCapture(0)
-    cap = cv2.VideoCapture("girls1.mp4")
+    cap = cv2.VideoCapture(0)
+    #cap = cv2.VideoCapture("girls1.mp4")
     cap.set(3, 640)
     cap.set(4, 480)
-    out = cv2.VideoWriter(
+    VideoWriter = cv2.VideoWriter(
         "output.avi", cv2.VideoWriter_fourcc(*"MJPG"), 10.0,
         (640, 480))
     print("Starting the YOLO loop...")
@@ -93,12 +97,12 @@ def YOLO():
         detections = darknet.detect_image(netMain, metaMain, darknet_image, thresh=0.25)
 
         image = cvDrawBoxes(detections, frame_read)        
-        
+        VideoWriter.write(image)
         print(f'FPS: {1/(time.time()-prev_time)}')
         cv2.imshow('Demo', image)
         if cv2.waitKey(1) & 0xff == 27:
             cap.release()
-            out.release()
+            VideoWriter.release()
             cv2.destroyAllWindows()
             break
 
